2ed32d882f1390cfbcdf20774ee0a01a66c17d5c
Rick Conrey <rickconrey@gmail.com>
2013-10-22 13:00:06 -0700

    updated views for view_student

--- crm/views.py
+++ crm/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+from crm.models import Student, Contact, Relationship
+
+def view_student(request, student_id):
+    try:
+        student = Student.objects.get(pk=student_id)
+    except Student.DoesNotExist:
+        raise Http404
+    return render(request, 'crm/view.html', {'student':student})
