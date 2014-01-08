9104a2769a9ae66b9b9a26ef86934ace05c065cd
Rick Conrey <rickconrey@gmail.com>
2014-01-08 08:59:30 -0800

    added Student and Contact UpdateView

--- crm/urls.py
+++ crm/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, url
+from crm.views import StudentUpdate, ContactUpdate
 
 from crm import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<student_id>\d+)/$', views.view_student, name='view_student'),
+    url(r'^(?P<student_id>\d+)/$', views.listing, name='listing'),
+    url(r'^update/(?P<pk>\d+)/$', StudentUpdate.as_view(),
+        name='student_update'),
+    url(r'^contactup/(?P<pk>\d+)/$', ContactUpdate.as_view(),
+        name='contact_update'),
+    #url(r'^(?P<student_id>\d+)/$', views.view_student, name='view_student'),
 )
--- crm/views.py
+++ crm/views.py
@@ -1,12 +1,32 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic.edit import UpdateView, ModelFormMixin
 from crm.models import Student, Contact, Relationship
 from crm.models import ContactEmail, ContactPhone
 from crm.models import StudentEmail, StudentPhone
 from crm.models import StatusStudent, Attendance
 from crm.models import Test, TestScore, Tip
 
-def view_student(request, student_id):
+def listing(request, student_id):
+    student_list = Student.objects.all()
+    paginator = Paginator(student_list, 1)
+
+    page = student_id
+    try:
+        student_page = paginator.page(page)
+    except PageNotAnInteger:
+        student_page = paginator.page(1)
+    except EmptyPage:
+        student_page = paginator.page(paginator.num_pages)
+
+    results = view_student(student_page.number)
+    results['student_page'] = student_page
+
+    return render(request, 'crm/view.html', results)
+
+
+def view_student(student_id):#request, student):#student_id):
     try:
         student = Student.objects.get(pk=student_id)
         age = student.get_age()
@@ -32,13 +52,43 @@ def view_student(request, student_id):
         
     except Student.DoesNotExist:
         raise Http404
-    return render(request, 'crm/view.html', {'student':student,
-                                             'age':age,
-                                             'classification':classification,
-                                             'contacts':contacts,
-                                             'contact_phone':contact_phone,
-                                             'contact_email':contact_email,
-                                             'status':status,
-                                             'attendance':attendance,
-                                             #'next_test_date':next_test_date,
-                                             })
+
+    student_info = {'student':student, 'age':age, 'status':status,
+                    'classification':classification, 'contacts':contacts,
+                    'contact_phone':contact_phone,
+                    'contact_email':contact_email, 'attendance':attendance,
+                    }
+
+    return student_info
+
+    #return render(request, 'crm/view.html', {'student':student,
+    #                                         'age':age,
+    #                                         'classification':classification,
+    #                                         'contacts':contacts,
+    #                                         'contact_phone':contact_phone,
+    #                                         'contact_email':contact_email,
+    #                                         'status':status,
+    #                                         'attendance':attendance,
+    #                                         #'next_test_date':next_test_date,
+    #                                         })
+
+
+class StudentUpdate(UpdateView):
+    model = Student
+    success_url = '/crm/1'   
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        Relationship.objects.filter(student=self.object).delete()
+        for contact in form.cleaned_data['contacts']:
+            relationship = Relationship()
+            relationship.student = self.object
+            relationship.contact = contact
+            relationship.save()
+        
+        return super(StudentUpdate, self).form_valid(form)
+
+class ContactUpdate(UpdateView):
+    model = Contact
+    success_url = '/crm/1'
+
